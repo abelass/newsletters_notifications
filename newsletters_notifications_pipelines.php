4d535f2decbbfcbdacba8aa3a46c3693f197d00f
Rainer Müller <rainer@websimple.be>
2014-09-26 16:51:51 +0200

    to do

--- newsletters_notifications_pipelines.php
+++ newsletters_notifications_pipelines.php
@@ -21,6 +21,8 @@ function newsletters_notifications_formulaire_traiter($flux){
 			 // Notifications
 
 		    if ($notifications = charger_fonction('notifications', 'inc', true)) {
+		    	
+				// To do choix des listes selon la config
 
 			    include_spip('inc/config');
 			    $options = array('config'=>lire_config('newsletters_notifications'),'email_inscription'=>_request('session_email'));
@@ -28,6 +30,8 @@ function newsletters_notifications_formulaire_traiter($flux){
 					
 				if ($listes AND is_string($listes))
 				$listes = explode(',',$listes);
+				
+				
 		
 		        // Envoyer aux admins
 		        $notifications('inscription_newsletter', $listes, $options);
